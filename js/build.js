b67e8b81406c2587503084bfa3fca73664777902
edwardsja <edw15013@byui.edu>
2021-10-20 07:32:52 -0400

    display user info in table

--- js/build.js
+++ js/build.js
@@ -1,8 +1,20 @@
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(function (response) { return response.json(); })
     .then(function (users) {
-    console.log(users);
+    var tbody = document.createElement('tbody');
+    for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+        var user = users_1[_i];
+        var tr = document.createElement('tr');
+        var userAddress = user.address.street + ", " + user.address.city + " " + user.address.zipcode;
+        var userInfo = [user.name, user.website, user.email, userAddress];
+        for (var i = 0; i < userInfo.length; i++) {
+            var td = document.createElement('td');
+            var textNode = document.createTextNode(userInfo[i]);
+            td.appendChild(textNode);
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    return document.getElementsByTagName('table')[0].appendChild(tbody);
 })
-    .catch(function (err) {
-    console.log(err);
-});
+    .catch(function (err) { console.log(err); });
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,8 +1,21 @@
 fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+    .then(response => { return response.json(); })
     .then(users => {
-        console.log(users);
+        let tbody = document.createElement('tbody');
+        for (let user of users) {
+            let tr = document.createElement('tr');
+            let userAddress = `${user.address.street}, ${user.address.city} ${user.address.zipcode}`;
+            let userInfo = [user.name, user.website, user.email, userAddress];
+
+            for (let i = 0; i < userInfo.length; i++) {
+                let td = document.createElement('td');
+                let textNode = document.createTextNode(userInfo[i]);
+                td.appendChild(textNode);
+                tr.appendChild(td);
+            }
+
+            tbody.appendChild(tr);
+        }
+        return document.getElementsByTagName('table')[0].appendChild(tbody);
     })
-    .catch(err => {
-        console.log(err);
-    });
+    .catch(err => { console.log(err); });
